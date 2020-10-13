59855c76eae2087380b23ddf51141c5a6bb31714
Taiki Hayashi <grandcolline@gmail.com>
2020-10-13 18:56:38 +0900

    add validation

--- variables.tf
+++ variables.tf
@@ -35,6 +35,11 @@ variable type {
   type        = string
   default     = "no"
   description = "fargate service type. load balancer or service discovery or nothing (lb/sd/no)"
+
+  validation {
+    condition     = can(regex("lb|sb|no", var.type))
+    error_message = "The type value must lb, sb or no."
+  }
 }
 
 variable assign_public_ip {
